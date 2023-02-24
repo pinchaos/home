0117129e323fc481b87f2986a360b08652356f80
pinchaos <126213262+pinchaos@users.noreply.github.com>
2023-02-23 22:48:22 -0500

    Fix img folder path

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -5,13 +5,13 @@ export const Styles = createGlobalStyle`
 
     @font-face {
         font-family: "Motiva Sans Light";
-        src: url("/fonts/Motiva-Sans-Light.ttf") format("truetype");
+        src: url("./fonts/Motiva-Sans-Light.ttf") format("truetype");
         font-style: normal;
     }
 
     @font-face {
         font-family: "Motiva Sans Bold";
-        src: url("/fonts/Motiva-Sans-Bold.ttf") format("truetype");
+        src: url("./fonts/Motiva-Sans-Bold.ttf") format("truetype");
         font-style: normal;
     }
 
